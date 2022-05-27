008da8d6949ba653f2af5ffcc8519c0ed961cfcf
GiovanniRezende <92121728+GiovanniRezende@users.noreply.github.com>
2022-05-27 09:09:09 -0300

    Add files via upload

---
+++ macakoesportivo/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="navbar">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <br>
+        <RouterLink to="/about">About</RouterLink>
+        <br>
+        <RouterLink to="/about">Jogadores</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+</style>
---
+++ macakoesportivo/src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+</script>
